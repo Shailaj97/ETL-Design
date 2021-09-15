4df1bd41e6b34e87b36fa9602c4573ce1f767d8a
Shailaj G.C <49581095+Shailaj97@users.noreply.github.com>
2021-09-15 10:33:13 +0530

    Add files via upload

---
+++ Assignment2/schema/raw_employee1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE raw_employee1(
+	employee_id VARCHAR(100),
+	first_name VARCHAR(100),
+	last_name VARCHAR(100),
+	department_id VARCHAR(100),
+	department_name VARCHAR(100),
+	manager_employee_id VARCHAR(100),
+	employee_role VARCHAR(100),
+	salary VARCHAR(100),
+	hire_date VARCHAR(100),
+	terminated_date VARCHAR(100),
+	terminated_reason VARCHAR(100),
+	dob VARCHAR(100),
+	fte VARCHAR(100),
+	location VARCHAR(100)
+
+);
